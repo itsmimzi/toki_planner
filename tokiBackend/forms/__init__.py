c0232b954ba624d30397aace208a67c3bb858953
Maryam Mouat <101988918+itsmimzi@users.noreply.github.com>
2024-04-24 11:54:52 -0400

    Delete tokiBackend directory

--- tokiBackend/forms/__init__.py
+++
@@ -1,4 +0,0 @@
-from .change_form import UserChangeForm
-from .createTask_form import createTaskForm
-from .login_form import UserLogInForm
-from .signup_form import UserSignUpForm
--- tokiBackend/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .category import Category
-from .priority import Priority
-from .weekdays import WeekDays
-from .task import Task
